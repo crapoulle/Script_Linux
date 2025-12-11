9afa4d0d5b7daec2ca26d3cae2815852bab68f1f
crapoulle <alexandre.lucashuit@gmail.com>
2025-12-11 21:30:20 +0100

    Organisation

--- process_users.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Fichier d'entrée
-INPUT="users.txt"
-
-# Traitement
-sed -e 's/users/standard_users/' \
-    -e 's/:\([0-9]\+\):/:ID:\1:/' \
-    -e '/^#/d' "$INPUT" | cut -d':' -f4
